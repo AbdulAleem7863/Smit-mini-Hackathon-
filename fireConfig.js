2d0e3dd256a3086dc7b17ef76e393a1bc5e42867
AbdulAleem7863 <abdulaleemnaqshbandi1234@gmail.comj>
2023-08-19 06:41:52 -0700

    first hackathon

---
+++ fireConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCv54UNaMD65eetAPSkZE3QUMX8nSGVIe0",
+    authDomain: "del-project-eeaa2.firebaseapp.com",
+    databaseURL: "https://del-project-eeaa2-default-rtdb.firebaseio.com",
+    projectId: "del-project-eeaa2",
+    storageBucket: "del-project-eeaa2.appspot.com",
+    messagingSenderId: "380845285206",
+    appId: "1:380845285206:web:20fe0c75e3eadfcb3b00f7"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
